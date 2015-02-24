1bab97a3ed7793f083c1334766d4b5a1c447787c
Rose Weixel <rose.weixel@gmail.com>
2015-02-24 14:43:09 -0500

    solution

--- lib/algorithms_heap.rb
+++ lib/algorithms_heap.rb
@@ -1,1 +1,29 @@
-# code your solution here
+class MinHeap
+  attr_accessor :root, :data
+
+  def initialize
+    @data = []
+  end
+
+  def push(number)
+    self.data << number
+    if !root || number < root
+      self.root = number
+    end
+  end
+
+  def pop
+    popped = @data.delete_at(-1)
+    if root == popped && !data.include?(root)
+      min = data[0]
+      data[1..-1].each do |num|
+        if num < min
+          min = num
+        end
+      end
+      self.root = min
+    end
+    popped
+  end
+
+end
