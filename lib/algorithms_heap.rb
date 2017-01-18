1cd7429385db9f19efa76aaa7a038453d14a542b
lshapz <laurashapiro01@gmail.com>
2017-01-18 13:56:31 -0500

    Done.

--- lib/algorithms_heap.rb
+++ lib/algorithms_heap.rb
@@ -1,1 +1,30 @@
 # code your solution here
+class MinHeap
+
+  attr_accessor :data, :root
+
+def initialize
+  @data = []
+end
+
+def push(item)
+  small = @data.select {|x| x < item}
+  if small.empty? 
+    self.root = item
+  #   self.data << item
+  # else
+  end
+    self.data << item
+end
+
+def pop
+  if self.data.last == self.root 
+    foo = self.data.pop
+    self.root = self.data.min 
+    foo
+  else
+    self.data.pop
+  end
+end
+
+end
